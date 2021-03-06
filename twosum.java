b8a5868097b9fe2c43d0aaf026afa6159845cde4
Vibhor Singh <vibhor.formal@gmail.com>
2021-03-06 09:25:40 +0530

    Updated

--- twosum.java
+++ twosum.java
@@ -1,10 +1,22 @@
-public int[] twoSum(int[] nums, int target) {
-    for (int i = 0; i < nums.length; i++) {
-        for (int j = i + 1; j < nums.length; j++) {
-            if (nums[j] == target - nums[i]) {
-                return new int[] { i, j };
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int x=0,y=0;
+        for(int i=0;i<=nums.length;i++)
+        {
+             for(int j=i+1;j<nums.length;j++)
+        {
+            if((nums[i]+nums[j])==target)
+            {
+                x=i;
+                y=j;
+                 break;
             }
+                //cout<<'['<<i<<','<<j<<']';
+                //System.out.println("["+i+","+j+"]");
+        } 
         }
+        int z[]={x,y};
+    return z;
     }
-    throw new IllegalArgumentException("No two sum solution");
+    
 }
